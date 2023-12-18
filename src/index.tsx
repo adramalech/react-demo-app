6e4773e6482c5f65c90af712a66907bac106ffc1
Jonathan Throne <adramalech707@gmail.com>
2023-12-18 08:42:56 -0800

    Fixed estlintrc.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
-import React from "react";
-import { App } from "./components/App";
-import { createRoot } from "react-dom/client";
+import React from 'react'
+import { App } from './components/App'
+import { createRoot } from 'react-dom/client'
 
-//createRoot(<App />, document.querySelector("#root"));
+const root = createRoot(document.querySelector('#root') as Element)
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-const root = createRoot(document.querySelector("#root"));
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
