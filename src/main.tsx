d70a98e171fedc58d437723216a2fa8e3c9d1259
Jonathan Throne <adramalech707@gmail.com>
2024-12-28 15:26:51 -0800

    Upgraded to vite bun and react v19.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import { App } from './App.tsx'
+import './main.css';
+import { App } from './app.tsx';
 
 const root = createRoot(document.querySelector('#root') as Element);
 
